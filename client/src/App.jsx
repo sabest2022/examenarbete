6c11800b322425a7b468c230fa816bce30128c12
sabest2022 <askarians@me.com>
2024-01-13 12:50:30 +0100

    remove content from App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import background from './assets/background.png';
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
@@ -9,7 +9,7 @@ import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="app" style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
@@ -17,13 +17,7 @@ function App() {
       <Header />
       <main style={{ flex: 1 }}>
 
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
 
-
-        </div>
       </main>
 
       <Footer />
